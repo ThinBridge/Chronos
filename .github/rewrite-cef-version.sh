5ed99f8317339f38538fb4be325781342d339d17
Takashi Hashida <hashida@clear-code.com>
2023-04-17 15:15:36 +0900

    Update CEF version to 112.2.9+g3303e87+chromium-112.0.5615.87 (#53)

--- .github/rewrite-cef-version.sh
+++ .github/rewrite-cef-version.sh
@@ -5,7 +5,7 @@ set -xe
 TOP_DIR=$(dirname $0)/..
 CEF_BAT=$TOP_DIR/setup-cef.bat
 
-VERSION=112.2.7+gef29713+chromium-112.0.5615.49
+VERSION=112.2.9+g3303e87+chromium-112.0.5615.87
 case $1 in
     stable)
 	TARGET_VERSION=$(curl --silent https://cef-builds.spotifycdn.com/index.json | jq --raw-output '.windows32.versions[] | select(.channel == "stable").cef_version' | head -n 1)
