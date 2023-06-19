743423a2d7ebbe3c8fc017f2491ee6ea6c1dc9c7
Takashi Hashida <hashida@clear-code.com>
2023-06-19 14:21:06 +0900

    Create CertificationDialog class

---
+++ CertificationDialog.cpp
@@ -0,0 +1,34 @@
+﻿// CertificationDialog.cpp : 実装ファイル
+//
+
+#include "pch.h"
+#include "Sazabi.h"
+#include "CertificationDialog.h"
+#include "afxdialogex.h"
+
+
+// CertificationDialog ダイアログ
+
+IMPLEMENT_DYNAMIC(CertificationDialog, CDialogEx)
+
+CertificationDialog::CertificationDialog(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DLG_CERTIFICATION, pParent)
+{
+
+}
+
+CertificationDialog::~CertificationDialog()
+{
+}
+
+void CertificationDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CertificationDialog, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CertificationDialog メッセージ ハンドラー
