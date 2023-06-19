f25a4c662561e7acc5b366a3d4b1b79cf947059f
Takashi Hashida <hashida@clear-code.com>
2023-06-19 14:38:21 +0900

    Move path of DlgCertification in filters

---
+++ DlgCertification.cpp
@@ -0,0 +1,33 @@
+﻿// DlgCertification.cpp : 実装ファイル
+//
+#include "stdafx.h"
+#include "Sazabi.h"
+#include "DlgCertification.h"
+#include "afxdialogex.h"
+
+
+// DlgCertification ダイアログ
+
+IMPLEMENT_DYNAMIC(DlgCertification, CDialogEx)
+
+DlgCertification::DlgCertification(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DLG_CERTIFICATION, pParent)
+{
+
+}
+
+DlgCertification::~DlgCertification()
+{
+}
+
+void DlgCertification::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(DlgCertification, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// DlgCertification メッセージ ハンドラー
---
+++ DlgCertification.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+// DlgCertification ダイアログ
+
+class DlgCertification : public CDialogEx
+{
+	DECLARE_DYNAMIC(DlgCertification)
+
+public:
+	DlgCertification(CWnd* pParent = nullptr);   // 標準コンストラクター
+	virtual ~DlgCertification();
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLG_CERTIFICATION };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
+
+	DECLARE_MESSAGE_MAP()
+};
