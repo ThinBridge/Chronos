8cdc5ceb510a4090e77a32babed805289f3451c7
Takashi Hashida <hashida@clear-code.com>
2023-06-20 17:00:48 +0900

    Update _APS_NEXT_RESOURCE_VALUE

--- resource.h
+++ resource.h
@@ -518,7 +518,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        229
+#define _APS_NEXT_RESOURCE_VALUE        435
 #define _APS_NEXT_COMMAND_VALUE         33015
 #define _APS_NEXT_CONTROL_VALUE         1094
 #define _APS_NEXT_SYMED_VALUE           104
