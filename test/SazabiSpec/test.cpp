c34f823caec03e40d2d12bd91a1bb481dd73b343
Takashi Hashida <hashida@clear-code.com>
2023-07-27 13:45:18 +0900

    Add gtest sample

---
+++ test/SazabiSpec/test.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "..\..\StdAfx.h"
+#include "..\..\Sazabi.h"
+#include "..\..\client_handler.h"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  ClientHandler handler;
+  EXPECT_TRUE(true);
+}
