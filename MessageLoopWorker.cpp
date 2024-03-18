95e04aa7412a3d0b85e861c31d6f4c8f070e6f91
Takashi Hashida <hashida@clear-code.com>
2024-03-18 16:59:07 +0900

    commit uncommitted changes

--- MessageLoopWorker.cpp
+++ MessageLoopWorker.cpp
@@ -12,15 +12,15 @@ MessageLoopWorker::MessageLoopWorker()
 	m_bTimerPending_ = false;
 	m_bIsActive_ = false;
 	m_bReentrancyDetected_ = false;
-	m_hWnd_ = NULL;
+	m_hWnd = NULL;
 }
 
 MessageLoopWorker::~MessageLoopWorker()
 {
 	KillTimer();
-	::SetWindowLongPtr(m_hWnd_, GWLP_USERDATA, NULL);
-	DestroyWindow(m_hWnd_);
-	m_hWnd_ = NULL;
+	::SetWindowLongPtr(m_hWnd, GWLP_USERDATA, NULL);
+	DestroyWindow(m_hWnd);
+	m_hWnd = NULL;
 }
 
 void MessageLoopWorker::Run()
@@ -65,14 +65,14 @@ void MessageLoopWorker::OnTimerTimeout()
 void MessageLoopWorker::SetTimer(int64_t delayMs)
 {
 	m_bTimerPending_ = true;
-	::SetTimer(m_hWnd_, m_nTimerID, static_cast<UINT>(delayMs), nullptr);
+	::SetTimer(m_hWnd, m_nTimerID, static_cast<UINT>(delayMs), nullptr);
 }
 
 void MessageLoopWorker::KillTimer()
 {
 	if (m_bTimerPending_)
 	{
-		::KillTimer(m_hWnd_, m_nTimerID);
+		::KillTimer(m_hWnd, m_nTimerID);
 		m_bTimerPending_ = false;
 	}
 }
@@ -102,11 +102,11 @@ void MessageLoopWorker::DoWork()
 	if (m_bReentrancyDetected_)
 	{
 		// Execute the remaining work as soon as possible.
-		PostMessage(m_hWnd_, WM_SCHEDULE_CEF_WORK, NULL, 0);
+		PostMessage(m_hWnd, WM_SCHEDULE_CEF_WORK, NULL, 0);
 	}
 	else if (!m_bTimerPending_)
 	{
-		PostMessage(m_hWnd_, WM_SCHEDULE_CEF_WORK, NULL, static_cast<LPARAM>(m_nTimerDelayPlaceholder));
+		PostMessage(m_hWnd, WM_SCHEDULE_CEF_WORK, NULL, static_cast<LPARAM>(m_nTimerDelayPlaceholder));
 	}
 }
 
@@ -116,14 +116,14 @@ void MessageLoopWorker::InitWindow()
 	HINSTANCE hInstance = theApp.m_hInstance;
 	WNDCLASSEX wcex = {};
 	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.lpfnWndProc = MessageLoopWorker::MessageLoopWindowHandler;
+	wcex.lpfnWndProc = MessageLoopWorker::WindowProcesser;
 	wcex.hInstance = hInstance;
 	wcex.lpszClassName = className;
 	RegisterClassEx(&wcex);
 
-	m_hWnd_ = CreateWindowW(className, nullptr, WS_OVERLAPPEDWINDOW, 0, 0, 0, 0,
+	m_hWnd = CreateWindowW(className, nullptr, WS_OVERLAPPEDWINDOW, 0, 0, 0, 0,
 					  HWND_MESSAGE, nullptr, hInstance, nullptr);
-	::SetWindowLongPtr(m_hWnd_, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+	::SetWindowLongPtr(m_hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
 }
 
 // static
