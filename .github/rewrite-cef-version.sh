deac87eb4e91ba6273c64505beb5ccf996559c8b
Takashi Hashida <hashida@clear-code.com>
2025-01-09 23:21:09 +0900

    Update CEF version to 131.4.1

--- .github/rewrite-cef-version.sh
+++ .github/rewrite-cef-version.sh
@@ -5,7 +5,7 @@ set -xe
 TOP_DIR=$(dirname $0)/..
 CEF_BAT=$TOP_DIR/setup-cef.bat
 
-VERSION=131.2.4+gb7543e4+chromium-131.0.6778.70
+VERSION=131.4.1+g437feba+chromium-131.0.6778.265
 case $1 in
     stable)
 	TARGET_VERSION=$(curl --silent https://cef-builds.spotifycdn.com/index.json | jq --raw-output '.windows32.versions[] | select(.channel == "stable").cef_version' | head -n 1)
