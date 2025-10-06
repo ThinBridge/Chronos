821892d119896220f779d1d667391acbdb3c4303
Takashi Hashida <hashida@clear-code.com>
2025-10-06 10:41:43 +0900

    Add feature to specify CEF commandline opption with config file (#324)
    * Add feature to specify CEF commandline opption with config file
    
    * Rename a parameter name and variable names

--- client_app.cpp
+++ client_app.cpp
@@ -24,6 +24,19 @@ void ClientApp::OnBeforeCommandLineProcessing(const CefString& process_type, Cef
 {
 	PROC_TIME(OnBeforeCommandLineProcessing)
 
+	// 追加のコマンドラインオプションはこの時点で反映する。
+	// 値付きSwitchは後勝ちなので、Chronosの設定により指定された値付きSwitchが優先されるようにするため。
+	CString commandLineFromConfig = theApp.m_AppSettings.GetCEFCommandLine();
+	if (!commandLineFromConfig.IsEmpty())
+	{
+		CefString originalCommandLine = command_line->GetCommandLineString();
+		CString newCommandLine((LPCWSTR)originalCommandLine.c_str());
+		newCommandLine.Append(_T(" "));
+		newCommandLine.Append(commandLineFromConfig);
+		CefString newCommandLineAsCefString(newCommandLine);
+		command_line->InitFromString(newCommandLineAsCefString);
+	}
+
 	//CEF131では、GoBackとGoForwardでキャッシュが有効だとFaviconが更新されない問題がある。
 	//そのため、キャッシュを無効化する。
 	//https://github.com/chromiumembedded/cef/issues/3874
